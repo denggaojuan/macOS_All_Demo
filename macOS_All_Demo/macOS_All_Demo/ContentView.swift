9cff8ad2e9837a60d40fa42a57d8ebb024504b32
邓高鹃 <1852022504@qq.com>
2020-09-09 18:35:00 +0800

    搭建项目基础设施

---
+++ macOS_All_Demo/macOS_All_Demo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  macOS_All_Demo
+//
+//  Created by 邓高鹃 on 2020/9/9.
+//  Copyright © 2020 邓高鹃. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
